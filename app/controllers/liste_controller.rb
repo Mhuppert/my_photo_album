34621039acc48f5cd780a65b31191423a57b2fca
Mhuppert <michel.huppert@free.fr>
2010-04-23 13:48:10 -0500

    after creation of page Menu

--- app/controllers/liste_controller.rb
+++ app/controllers/liste_controller.rb
@@ -1,5 +1,8 @@
 class ListeController < ApplicationController
   def index
+    
+    @pages = Page.find_pages_for_display
+    
   end
 
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,10 @@
 class Page < ActiveRecord::Base
   
   has_many :chapters, :dependent  => :destroy
+  
+  def self.find_pages_for_display
+    find(:all, :order => "date_page")
+  end
     
 
 end
